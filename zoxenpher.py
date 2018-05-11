bd206073c4fbeb1893f34ecebe22a2cdc61c3381
Thomas <teleathers22@gmail.com>
2018-05-11 19:51:43 -0400

    move some auxilary functions into new libzox library.

--- zoxenpher.py
+++ zoxenpher.py
@@ -12,72 +12,14 @@ print("Zoxenpher v2.0.1")
 #number of images loaded for preview. (images not previewed will be shown as links)
 maximages=10
 
-def pathfigure(host, port, selector, gtype="0"):
-	if host.startswith("about:"):
-		hoststripped=host.replace('\\', "").replace("/", "").replace("..", "")
-		hoststripped=hoststripped[6:]
-		if os.path.isfile(os.path.join("vgop", hoststripped)):
-			data=open(os.path.join("vgop", hoststripped))
-		else:
-			if gtype=="1":
-				data=open(os.path.join("vgop", "gaierror"))
-			if gtype=="0":
-				data=open(os.path.join("vgop", "gaierror.txt"))
-			if gtype=="p":
-				data=open(os.path.join("vgop", "gaierror.png"))
-			if gtype=="I" or gtype=="g":
-				data=open(os.path.join("vgop", "gaierror.gif"))
-	else:
-		try:
-			data=libgop.gopherget(host, port, selector)
-		except Exception as err:
-			print(err)
-			if gtype=="1":
-				data=open(os.path.join("vgop", "gaierror"))
-			if gtype=="0":
-				data=open(os.path.join("vgop", "gaierror.txt"))
-			if gtype=="p":
-				data=open(os.path.join("vgop", "gaierror.png"))
-			if gtype=="I" or gtype=="g":
-				data=open(os.path.join("vgop", "gaierror.gif"))
-	return data
-
-
+import libzox
+from libzox import pathfigure
+from libzox import textitem
+from libzox import progobj
+from libzox import pathprogobj
+from libzox import imgget
 simplefont = pygame.font.SysFont("mono", 15)
-def textitem(text, xfont, yjump, textcolx, surface, ypos, renderdict):
-	xpos=0
-	rectlist=[]
-	words=text.split(" ")
-	if True:
-		buffstring=""
-		for word in words+[" ", None]:
-			
-			#print buffstring
-			if word==None:
-				dictkey=str(textcolx[0])+str(textcolx[1])+str(textcolx[2])+buffstring
-				if dictkey in renderdict:
-					namelabel=renderdict[dictkey]
-				else:
-					namelabel=xfont.render(buffstring, True, textcolx, (255, 255, 255))
-					renderdict[dictkey]=namelabel
-				rectlist.extend([surface.blit(namelabel, (xpos, ypos))])
-				ypos+=yjump
-			elif xfont.size(buffstring+word+" ")[0]<=surface.get_width():
-				buffstring+=word+" "
-			else:
-				dictkey=str(textcolx[0])+str(textcolx[1])+str(textcolx[2])+buffstring
-				if dictkey in renderdict:
-					namelabel=renderdict[dictkey]
-				else:
-					namelabel=xfont.render(buffstring, True, textcolx, (255, 255, 255))
-					renderdict[dictkey]=namelabel
-				rectlist.extend([surface.blit(namelabel, (xpos, ypos))])
-				ypos+=yjump
-				buffstring=word+" "
-	if len(rectlist)>0:
-		return (rectlist[0].unionall(rectlist), ypos, renderdict)
-	else:
-		return (pygame.Rect(0, 0, 0, 0), ypos, renderdict)
+
 
 class deskclass:
 	def __init__(self, progs):
@@ -176,57 +118,10 @@ class deskclass:
 		surface.blit(self.mascot, (surface.get_width()-self.mascot.get_width(), 0))
 
 
-class progobj:
-	def __init__(self, classref, icon, idcode, friendly_name, commname, xsize, ysize, resizable=0, key=None, mod=None, hint=""):
-		self.idcode=idcode
-		self.friendly_name=friendly_name
-		self.commname=commname
-		self.icon=icon
-		self.xsize=xsize
-		self.ysize=ysize
-		self.classref=classref
-		self.resizable=resizable
-		self.key=key
-		self.mod=mod
-		self.hint=hint
 
-
-class pathprogobj:
-	def __init__(self, classref, icon, idcode, friendly_name, commname, xsize, ysize, resizable=0, key=None, mod=None, host="about:splash", port=70, selector="/", hint=""):
-		self.idcode=idcode
-		self.friendly_name=friendly_name
-		self.commname=commname
-		self.icon=icon
-		self.xsize=xsize
-		self.ysize=ysize
-		self.classrefx=classref
-		self.resizable=resizable
-		self.key=key
-		self.mod=mod
-		self.host=host
-		self.port=port
-		self.selector=selector
-		self.hint=hint
-	def classref(self):
-		return self.classrefx(host=self.host, port=self.port, selector=self.selector)
 
 #run in a thread for each time a page is rendered the first time.
-def imgget(items, uptref, frameobj):
-	for mitem in items:
-		data=pathfigure(mitem.hostname, mitem.port, mitem.selector, gtype=mitem.gtype)
-		try:
-			if mitem.gtype=="g":
-				imagefx=pygame.image.load(data, "quack.gif")
-			if mitem.gtype=="p":
-				imagefx=pygame.image.load(data, "quack.png")
-			if mitem.gtype=="I":
-				imagefx=pygame.image.load(data)
-			imagefx.convert()
-			mitem.image=imagefx
-		except pygame.error:
-			return
-	if uptref!=None:
-		uptref(frameobj)
+
 
 
 def textshow(host, port, selector):
